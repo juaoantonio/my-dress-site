a82501f384cde1ff1bf0912b00bf057c792408e6
João Antônio Barbosa <joaobarbosadev@gmail.com>
2022-05-18 17:44:25 -0300

    Added VestidoListView

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import Vestido
 
 
 class LandingPageView(TemplateView):
     template_name = "landing_page/index.html"
 
+
+class VestidoListView(ListView):
+    model = Vestido
+    template_name = "landing_page/vestidos.html"
 
