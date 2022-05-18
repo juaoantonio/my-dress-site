e52c4086aa3029f5ec8f6558c9a1f312456445a1
João Antônio Barbosa <joaobarbosadev@gmail.com>
2022-05-18 18:15:55 -0300

    Setted Staticfiles config

--- core/settings.py
+++ core/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
