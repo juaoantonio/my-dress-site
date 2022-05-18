5b8156c1f5fc81b23c79bd929c0493461265246c
João Antônio Barbosa <joaobarbosadev@gmail.com>
2022-05-18 19:09:48 -0300

    Installed and configured python-decouple

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from email.policy import default
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@^12l38zftqvm5$a5qzydvu=_ly%o4*q*8cx42ok-^lsczp(n%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False , cast=bool)
 
 ALLOWED_HOSTS = []
 
